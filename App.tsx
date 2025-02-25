7be59f64dfcea0bd0dc081997811c541daee2b78
julcavasquez <jvandersson93@gmail.com>
2025-02-25 14:02:47 -0500

    Proyecto app Mi Mundo de Cuentos

---
+++ App.tsx
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+// Only import react-native-gesture-handler on native platforms
+import 'react-native-gesture-handler';
+
+
+
+function App() {
+
+
+  return (
+    <View>
+      <Text>Hola Mundo</Text>
+    </View>
+  );
+}
+
+
+export default App;
