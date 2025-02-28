625d3ebcdbc1091693b0eb3946d2f066114cbca7
jv <jvandersson93@gmail.com>
2025-02-28 18:49:56 -0500

    update navegacion

--- screens/Login.js
+++ screens/Login.js
@@ -4,7 +4,7 @@ import { Text, StyleSheet, View } from 'react-native'
 export default function Login() {
     return (
       <View>
-        <Text> Login </Text>
+        <Text> Pagina de Login </Text>
       </View>
     )
 }
